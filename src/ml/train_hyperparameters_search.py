5cb2cc5f3c65583229b50bc0996d8782837db966
Sebastian Jinich <sjinich@rho.iib.unsam.edu.ar>
2024-02-20 18:42:36 +0000

    train cheml2581

--- src/ml/train_hyperparameters_search.py
+++ src/ml/train_hyperparameters_search.py
@@ -2,7 +2,7 @@ import pandas as pd
 from ray import tune
 from engine.hyperparameters_tune_raytune import deepscreen_hyperparameter_tuneing
 
-data = pd.read_csv("../../.data/processed/CHEMBL5567.csv")
+data = pd.read_csv("../../.data/processed/CHEMBL2581.csv")
 search_space_deepscreen = {
         'fully_layer_1': tune.choice([16, 32, 128, 256, 512]),
         'fully_layer_2': tune.choice([16, 32, 128, 256, 512]),
@@ -10,7 +10,7 @@ search_space_deepscreen = {
         'batch_size': tune.choice([32, 64]),
         'drop_rate': tune.choice([0.2, 0.3, 0.5, 0.6]),
     }
-tuner = deepscreen_hyperparameter_tuneing(data,search_space_deepscreen,"chembl5567",num_samples=350)
+tuner = deepscreen_hyperparameter_tuneing(data,search_space_deepscreen,"CHEMBL2581",num_samples=50)
 result = tuner.tune_deepscreen()
 result_df = result.get_dataframe()
-result_df.to_pickle("../../.experiments/raytune_asha.pkl")
+result_df.to_pickle("../../.experiments/raytune_asha_chembl2581.pkl")
