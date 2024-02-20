05b5b2761fbc5a2cc454cbdcc67f668ee53217fd
Sebastian Jinich <sjinich@rho.iib.unsam.edu.ar>
2024-02-20 17:07:03 +0000

    posible fix de metricas

--- src/ml/engine/system.py
+++ src/ml/engine/system.py
@@ -3,8 +3,7 @@ import torch
 from torch.nn import functional as F
 import torch.nn as nn
 import lightning as L
-from torchmetrics import classification
-import torchmetrics
+from torchmetrics import classification, MetricCollection
 import pandas as pd
 import numpy as np
 import os
@@ -40,21 +39,35 @@ class DEEPScreenClassifier(L.LightningModule):
         self.config = configs
         
         # Performance trackers
-        self.train_acc = classification.BinaryAccuracy()
-        self.val_acc = classification.BinaryAccuracy()
-        self.test_acc = classification.BinaryAccuracy()
-
-        self.train_pres = classification.BinaryPrecision()
-        self.val_pres = classification.BinaryPrecision()
-        self.test_pres = classification.BinaryPrecision()
-
-        self.train_f1 = classification.BinaryF1Score()
-        self.val_f1 = classification.BinaryF1Score()
-        self.test_f1 = classification.BinaryF1Score()
-
-        self.train_mcc = classification.BinaryMatthewsCorrCoef()
-        self.val_mcc = classification.BinaryMatthewsCorrCoef()
-        self.test_mcc = classification.BinaryMatthewsCorrCoef()
+        self.train_metrics = MetricCollection(
+             {
+                "train_acc": classification.BinaryAccuracy(),
+                "train_prec": classification.BinaryPrecision(),
+                "train_f1": classification.BinaryF1Score(),
+                "train_mcc": classification.BinaryMatthewsCorrCoef(),
+                "train_recall": classification.BinaryRecall(),
+             }
+         )
+        
+        self.val_metrics = MetricCollection(
+             {
+                "val_acc": classification.BinaryAccuracy(),
+                "val_prec": classification.BinaryPrecision(),
+                "val_f1": classification.BinaryF1Score(),
+                "val_mcc": classification.BinaryMatthewsCorrCoef(),
+                "val_recall": classification.BinaryRecall(),
+             }
+         )
+        
+        self.test_metrics = MetricCollection(
+             {
+                "test_acc": classification.BinaryAccuracy(),
+                "test_prec": classification.BinaryPrecision(),
+                "test_f1": classification.BinaryF1Score(),
+                "test_mcc": classification.BinaryMatthewsCorrCoef(),
+                "test_recall": classification.BinaryRecall(),
+             }
+         )
 
         # Predictions
         self.test_predictions = pd.DataFrame()
@@ -84,53 +97,43 @@ class DEEPScreenClassifier(L.LightningModule):
         y_pred = self.forward(img_arrs)
         _, preds = torch.max(y_pred,1)
         loss = self.cross_entropy_loss(y_pred.squeeze(),label)
-        
-        # performance metrics
-        self.train_acc(preds,label)
-        self.train_f1(preds,label)
-        self.train_mcc(preds,label)
-        self.train_pres(preds,label)
-        
         self.log('train_loss', loss, batch_size=self.hparams.batch_size, prog_bar=True)
-        self.log('train_acc', self.train_acc, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size, prog_bar=True)
-        self.log('train_f1', self.train_f1, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
-        self.log('train_mcc', self.train_mcc, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size, prog_bar=True)
-        self.log('train_pres', self.train_pres, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
+
+        output = self.train_metrics(preds.int(),label.int())
+        self.log_dict(output,on_step=False,on_epoch=True, batch_size=self.hparams.batch_size)
+        self.train_metrics.update(preds.int(),label.int())
+        
         return loss
+    
+    def on_training_epoch_end(self, outputs):
+        self.log_dict(self.train_metrics.compute(), on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
+        self.train_metrics.reset()
     
     def validation_step(self,val_batch,batch_idx):
         img_arrs, label, comp_id = val_batch
         y_pred = self.forward(img_arrs)
         _, preds = torch.max(y_pred,1)
         loss = self.cross_entropy_loss(y_pred.squeeze(),label)
-        
-        self.val_acc(preds,label)
-        self.val_f1(preds,label)
-        self.val_mcc(preds,label)
-        self.val_pres(preds,label)
-        
-        self.log('val_loss', loss, batch_size=self.hparams.batch_size, sync_dist=True)
-        self.log('val_acc', self.val_acc, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
-        self.log('val_f1', self.val_f1, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
-        self.log('val_mcc', self.val_mcc, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
-        self.log('val_pres', self.val_pres, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
+        self.log('val_loss', loss, batch_size=self.hparams.batch_size, prog_bar=True)
+
+        output = self.val_metrics(preds.int(),label.int())
+        self.log_dict(output,on_step=False,on_epoch=True,batch_size=self.hparams.batch_size)
+        self.val_metrics.update(preds.int(),label.int())
+    
+    def on_validation_epoch_end(self):
+        self.log_dict(self.val_metrics.compute(), on_step=False, on_epoch=True,batch_size=self.hparams.batch_size)
+        self.val_metrics.reset()
 
     def test_step(self,test_batch,batch_idx):
         img_arrs, label, comp_id = test_batch
         y_pred = self.forward(img_arrs)
         _, preds = torch.max(y_pred,1)
         loss = self.cross_entropy_loss(y_pred.squeeze(),label)
-        
-        self.test_acc(preds,label)
-        self.test_f1(preds,label)
-        self.test_mcc(preds,label)
-        self.test_pres(preds,label)
+        self.log('test_loss', loss, batch_size=self.hparams.batch_size, prog_bar=True)
 
-        self.log('test_loss', loss, batch_size=self.hparams.batch_size, on_epoch=True)
-        self.log('test_acc', self.test_acc, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
-        self.log('test_f1', self.test_f1, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
-        self.log('test_mcc', self.test_mcc, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
-        self.log('test_pres', self.test_pres, on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
+        output = self.test_metrics(preds.int(),label.int())
+        self.log_dict(output,on_step=False,on_epoch=True,batch_size=self.hparams.batch_size)
+        self.test_metrics.update(preds.int(),label.int())
 
         comp_id_pd = pd.Series(comp_id,name="comp_id")
         label_pd = pd.Series(label,name="label")
@@ -141,11 +144,14 @@ class DEEPScreenClassifier(L.LightningModule):
         batch_predictions = pd.concat([comp_id_pd,label_pd,pred_pd,pred_0_pd,pred_1_pd],axis=1)
         self.test_predictions = pd.concat([self.test_predictions,batch_predictions],axis=0)
 
-
     def on_test_end(self):
         self.test_predictions["abs_prob_diff"] =  self.test_predictions["0_inactive_probability"] - self.test_predictions["1_active_probability"]
         self.test_predictions["abs_prob_diff"] = abs(self.test_predictions["abs_prob_diff"])
         self.test_predictions.to_csv(os.path.join(self.hparams.experiment_result_path,f"test_predictions_{self.hparams.fully_layer_1}-{self.hparams.fully_layer_2}-{self.hparams.learning_rate}-{self.hparams.drop_rate}-{self.hparams.batch_size}.csv"),index=False)
+    
+    def on_test_epoch_end(self):
+        self.log_dict(self.test_metrics.compute(), on_step=False, on_epoch=True, batch_size=self.hparams.batch_size)
+        self.test_metrics.reset()
 
     def predict_step(self, batch, batch_idx, dataloader_idx=None):
         img_arrs, comp_id = batch
