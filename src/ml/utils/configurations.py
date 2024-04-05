a71897b79dfca042f0c70431c47935d5cc85d17d
Sebastian Jinich <sjinich@rho.iib.unsam.edu.ar>
2024-04-05 13:53:46 +0000

    condiguraciones para poster

--- src/ml/utils/configurations.py
+++ src/ml/utils/configurations.py
@@ -37,11 +37,11 @@ class configurations:
     
     def get_hyperparameters_search_setup(self):
         setup = {
-           "max_epochs": 195,
+           "max_epochs": 200,
            "grace_period": 55,
            "metric_to_optimize": "val_mcc",
            "optimize_mode":"max",
-           "num_samples":450,
+           "num_samples":250,
            "asha_reduction_factor":3,
            "number_ckpts_keep":1
         }
